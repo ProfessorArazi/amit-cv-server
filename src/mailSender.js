d792ec5eaf8f6b7514b572a9289bcb097d1b0bc3
Amit <amitarazi12345@gmail.com>
2022-04-03 13:53:41 +0300

    added dotenv

--- src/mailSender.js
+++ src/mailSender.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 const mailSender = (name, contact, message) => {
   const email = process.env.MAIL;
   const me = process.env.ME;
+  console.log(email);
+  console.log(process.env.PASS);
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -20,11 +22,10 @@ const mailSender = (name, contact, message) => {
     ${message}</p> `,
   };
 
-  return transporter.sendMail(mailOptions, function (error, info) {
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     }
-    transporter.close();
   });
 };
 
