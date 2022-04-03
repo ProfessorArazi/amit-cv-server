fe5259708666c6b97feea6d3f8aedfd68abb7a70
Amit <amitarazi12345@gmail.com>
2022-04-03 18:52:55 +0300

    changed nodemailer to sgmail

--- src/mailSender.js
+++ src/mailSender.js
@@ -1,50 +1,29 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 
-const mailSender = async (name, contact, message) => {
+const mailSender = (name, contact, message) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
   const email = process.env.MAIL;
   const me = process.env.ME;
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
-    auth: {
-      user: email,
-      pass: process.env.PASS,
-    },
-    secure: true,
-  });
 
-  await new Promise((resolve, reject) => {
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log("Server is ready to take our messages");
-        resolve(success);
-      }
-    });
-  });
-
-  const mailOptions = {
-    from: email,
+  const msg = {
     to: me,
+    from: email,
     subject: "Contact via portfolio",
     html: `<p dir='ltr'>Name of contact : ${name} <br/> 
     Contact email : ${contact} <br/>
     ${message}</p> `,
   };
 
-  await new Promise((resolve, reject) => {
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        reject(err);
-      } else {
-        console.log(info);
-        resolve(info);
-      }
+  sgMail
+    .send(msg)
+    .then((response) => {
+      console.log(response[0].statusCode);
+      console.log(response[0].headers);
+    })
+    .catch((error) => {
+      console.error(error.response.body.errors);
     });
-  });
 };
 
 module.exports = mailSender;
