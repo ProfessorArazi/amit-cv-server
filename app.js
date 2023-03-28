9230040ed439130fb30179bd5239be5783139d12
Amit Arazi <amitarazi123456@gmail.com>
2023-03-28 15:35:36 +0300

    fix email

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: __dirname + "/.env" });
 const validator = require("validator");
-const nodemailer = require("nodemailer");
+const mailSender = require("./src/mailSender");
 
 app.use(express.json());
 app.use(cors({ origin: "*" }));
@@ -16,34 +16,6 @@ app.use(function (req, res, next) {
 app.get("/", (req, res) => {
   res.send({ message: "listening" });
 });
-
-const mailSender = (name, contact, message) => {
-  const email = process.env.MAIL;
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: email,
-      pass: process.env.PASS,
-    },
-  });
-
-  const me = process.env.ME;
-
-  const mailOptions = {
-    from: email,
-    to: me,
-    subject: "Contact via portfolio",
-    html: `<p dir='ltr'>Name of contact : ${name} <br/> 
-    Contact email : ${contact} <br/>
-    ${message}</p> `,
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    }
-  });
-};
 
 app.post("/contact", (req, res) => {
   const { name, email, message } = req.body;
