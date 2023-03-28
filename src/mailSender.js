ce0fdc1cc71f3d7d7ff2b25a92a199b69f614a21
Amit Arazi <amitarazi123456@gmail.com>
2023-03-28 16:57:15 +0300

    fix email

--- src/mailSender.js
+++ src/mailSender.js
@@ -3,20 +3,17 @@ const nodemailer = require("nodemailer");
 const mailSender = (name, contact, message) => {
   const email = process.env.MAIL;
   const transporter = nodemailer.createTransport({
-    host: "smtp.mail.yahoo.com",
+    service: "gmail",
     port: 465,
-    service: "yahoo",
-    secure: false,
+    secure: true, 
     auth: {
       user: email,
       pass: process.env.PASS,
     },
-    debug: false,
-    logger: true,
   });
-
+  
   const me = process.env.ME;
-
+  
   const mailOptions = {
     from: email,
     to: me,
@@ -25,10 +22,10 @@ const mailSender = (name, contact, message) => {
     Contact email : ${contact} <br/>
     ${message}</p> `,
   };
-
+  
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
-      console.log(error);
+     console.log(error)
     }
   });
 };
